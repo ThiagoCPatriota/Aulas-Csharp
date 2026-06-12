6f0d43d8684444666ae5f8e82a641015417c1d5c
Thiago Patriota <thiagosilvapatriota@gmail.com>
2026-06-12 20:03:49 -0300

    feat: Criação da Class livro e Adição de seus constutores.

--- Aulaobjetos.cs
+++ Aulaobjetos.cs
@@ -1,5 +1,25 @@
 public class Aluno
 {
-    public String Nome;
-    public int Idade;
+    public String Nome { get; }
+    public int Idade { get; }
+    public Aluno(String nome, int idade)
+    {
+        Nome = nome;
+        Idade = idade;
+    }
+}
+
+
+public class livro
+{
+    public String Titulo;
+    public String Autor;
+    public int Ano;
+
+    public livro(String titulo, String autor, int ano)
+    {
+        Titulo = titulo;
+        Autor = autor;
+        Ano = ano;
+    }
 }
